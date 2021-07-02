823fbedce1d4be087bf3cb46d415fe4b7d489d1a
Alex Skobelev <alex@alazy.dev>
2021-07-01 23:41:25 -0700

    fixes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const jwt = require("jsonwebtoken")
-const bodyParser = require("body-parser")
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-const allowedDomains = process.env.ALLOWED_DOMAINS.split(",")
-app.use(cors())
+app.use(cors());
 
-const port = 8000
-const app_secret = process.env.APP_SECRET
-
-
+const port = process.env.PORT;
+const app_secret = process.env.APP_SECRET;
 
 app.get("/", (req, res) => {
-  let payload = req.body
-  if(!payload) res.send("no payload found")
-
-  res.send(jwt.sign(payload, app_secret))
-})
+  let payload = req.body;
+  if (!typeof payload === "object" || Object.keys(payload).length === 0) {
+    res.status(400).send("no payload found");
+  } else {
+    let token = jwt.sign(payload, app_secret);
+    res.send(token);
+  }
+});
 
 app.post("/", (req, res) => {
-  let payload = req.body
-  let jwt = jwt.sign(payload, app_secret)
-  res.send(jwt)
-})
+  let payload = req.body;
+  if (!typeof payload === "object" || Object.keys(payload).length === 0) {
+    res.status(400).send("no payload found");
+  } else {
+    let token = jwt.sign(payload, app_secret);
+    res.send(token);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`listening http://localhost:${port}`)
-})
+  console.log(`listening http://localhost:${port}`);
+});
